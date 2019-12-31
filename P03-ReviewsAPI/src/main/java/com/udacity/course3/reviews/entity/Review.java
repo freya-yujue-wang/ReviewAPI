84df7145cf5bcf571e38e4c2a02b4fefaed6c8ac
wyjsusan <yujuewa@gmail.com>
2019-12-31 11:07:07 -0800

    Finish requirements of project: https://review.udacity.com/#!/rubrics/2650/view 1. contains the JPA entities for Product, Review & Comment. contains JPA entities which are connected using JPA collections. contains Spring Data JPA repositories for Product, Review & Comment entities.
    2. contains the necessary SQL scripts to create review, comment and product tables. contains the SQL to create foreign key constraints on review & comment tables. Review is related to Product. Comment is related to Review.
    
    3. Create new query: findReviewsByProductName.
    
    4. The project has tests for most of the methods in the Spring Data JPA repositories. Tests run against a H2 database.

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/entity/Review.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/entity/Review.java
@@ -1,19 +1,41 @@
 package com.udacity.course3.reviews.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String title;
-    private String content;
-    @ManyToOne
-    private Product product;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String title;
+  private String content;
+
+  @ManyToOne
+  @JoinColumn(name = "product_id", nullable = false)
+  @JsonIgnore
+  private Product product;
+
+  @OneToMany(mappedBy = "review")
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private List<Comment> comments;
+
+  public Review(int id, String title, String content) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
 }
--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepository.java
@@ -2,10 +2,15 @@ package com.udacity.course3.reviews.repository;
 
 import com.udacity.course3.reviews.entity.Product;
 import com.udacity.course3.reviews.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
-    List<Review> findAllByProduct(Product product);
+
+  @Query("select new Review(id, title, content) from Review where product.productName=:productName")
+  List<Review> findAllByProductName(String productName);
+
+  List<Review> findAllByProduct(Product product);
 }
