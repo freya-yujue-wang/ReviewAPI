40300a578c8a2146be30d5bb616139e0420fe8b3
wyjsusan <yujuewa@gmail.com>
2020-01-18 16:11:52 -0800

    fix the problem with JPA test and package could be packaged

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories
+//with this EnableMongoRepositories annotation, the JPA test cannot run.
+//@EnableMongoRepositories
 @SpringBootApplication
 public class ReviewsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReviewsApplication.class, args);
     }
+
+
 }
